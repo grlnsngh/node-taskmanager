830d17e87bbb160faf9178258b2abec64a619aa8
Gurleen Singh <grlnsngh@gmail.com>
2021-08-26 14:55:53 -0400

    updated database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://gurleen:qwertyuiop@cluster0.pdyen.mongodb.net/Cluster0?retryWrites=true&w=majority'}
-} else {
-  module.exports = {mongoURI: 'mongodb+srv://gurleen:qwertyuiop@cluster0.pdyen.mongodb.net/Cluster0?retryWrites=true&w=majority'}
-}
